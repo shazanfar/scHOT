759ad66a194e163682932f407f0d61b0acbda68b
yingxinlin <yingxinlindsay@gmail.com>
2020-02-06 19:16:12 +1100

    add plot order function

--- R/scHOT_plot.R
+++ R/scHOT_plot.R
@@ -64,7 +64,7 @@ plotColouredExpression = function(scHOT,
   } else {
 
     if (!(ranked_by %in% colnames(SummarizedExperiment::colData(scHOT)))) {
-      stop("branches provided is not found in colData(scHOT)")
+      stop("ranked_by provided is not found in colData(scHOT)")
     }
 
     branchData <- branchData[, order(SummarizedExperiment::colData(scHOT)[, ranked_by])]
@@ -104,9 +104,9 @@ plotColouredExpression = function(scHOT,
 
 
       gdf_list_1$ordercut = cut(gdf_list_1$order, n, labels = unlist(ifelse(n == 3,
-                                                                     list(c("Early", "Middle", "Late")),
-                                                                     ifelse(n == 2, list(c("Early", "Late")),
-                                                                            list(paste0("Group ", 1:n))))))
+                                                                            list(c("Early", "Middle", "Late")),
+                                                                            ifelse(n == 2, list(c("Early", "Late")),
+                                                                                   list(paste0("Group ", 1:n))))))
     } else {
       gdf_list_1$ordercut = rep("All cells", times = nrow(gdf_list_1))
     }
@@ -124,7 +124,7 @@ plotColouredExpression = function(scHOT,
     gdf_sub = gdf
   }
   g = ggplot2::ggplot(gdf_sub, aes(x = gdf_sub$ExpressionGene1, y = gdf_sub$ExpressionGene2,
-                          color = order)) +
+                                   color = order)) +
     geom_point(show.legend = FALSE, alpha = 0.7) +
     facet_grid(branch ~ ordercut, scales = "free_y") +
     scale_color_gradientn(colours = c("orange", "blue")) +
@@ -138,5 +138,154 @@ plotColouredExpression = function(scHOT,
   }
   return(g)
 }
+
+
+
+##############################################
+
+#' the plotOrderedExpression function plots expression vectors along branches and genes as ribbon plots
+#'
+#' @title plotOrderedExpression
+#' @param scHOT A scHOT object, where the expression data is stored in the assay slot, with assay name "expression".
+#' @param genepair is either a single character string with an underscore, or a two length character vector
+#' @param branches A character indicates that the colnames stored the branch information in colData
+#' @param ranked_by A character indicates that the colnames stored the ranking information of the cells in colData, such as trajectory time
+#' @param xvals A character indicates that the colnames stored in colData of the x-values associated with the samples in branchData
+#' @param subsetBranch subsetBranch is a character vector containing the names of the branches to be plotted. If NULL it will plot all branches
+#' @param facet can either be FALSE, "branch", "gene", or "both"
+#' @return \code{ggplot} a ggplot object for ribbon plot with points
+#'
+#'
+#' @importFrom SummarizedExperiment assay colData
+#' @import ggplot2
+#'
+#' @export
+#'
+plotOrderedExpression = function(scHOT,
+                                 genepair,
+                                 branches = NULL,
+                                 ranked_by = NULL,
+                                 xvals = NULL,
+                                 subsetBranch = NULL,
+                                 facet = FALSE) {
+
+  # branchData is a list containing matrices of the cell expression per branch
+  # assumed that the columns of each matrix in branchData is ordered by pseudotime
+  # if branchData is not given as a list, it will be converted into a list containing branchData
+  # gene is either a single character string with an underscore, or a two length character vector
+  # xvals is a list containing the x-values associated with the samples in branchData (if NULL, samples will just be plotted against their rank)
+  # subsetBranch is a character vector containing the names of the branches to be plotted. If NULL it will plot all branches
+  # facet can either be FALSE, "branch", "gene", or "both"
+
+
+  if (length(genepair) == 1) {
+    genepair = unlist(strsplit(genepair, "_"))
+  }
+  else {
+    genepair = genepair[1:2]
+  }
+
+  if (length(genepair) != 2) {
+    stop("genepair is either a single character string with an underscore, or a two length character vector")
+  }
+
+  branchData <- SummarizedExperiment::assay(scHOT, "expression")
+
+
+
+
+  if (is.null(ranked_by)) {
+    message("ranked_by information is not provided, the expression data is ranked by the branches")
+  } else {
+
+    if (!(ranked_by %in% colnames(SummarizedExperiment::colData(scHOT)))) {
+      stop("ranked_by provided is not found in colData(scHOT)")
+    }
+
+    branchData <- branchData[, order(SummarizedExperiment::colData(scHOT)[, ranked_by])]
+  }
+
+
+
+  if (is.null(branches)) {
+    message("branches information is not provided")
+    branchData = list(Branch = branchData)
+  } else {
+    if (!(branches %in% colnames(colData(scHOT)))) {
+      stop("branches provided is not found in colData(scHOT)")
+    }
+
+    branch_info <- SummarizedExperiment::colData(scHOT)[, branches]
+
+    branchData <- lapply(split(seq_along(branch_info), branch_info),
+                         function(idx) branchData[, idx])[order(unique(branch_info))]
+  }
+
+
+
+
+  gdf_list = sapply(genepair, function(g) {
+
+    gdf = do.call(rbind,lapply(branchData, function(branch){
+      gdf_list_1 = data.frame(
+        Sample = colnames(branch),
+        order = 1:ncol(branch),
+        ExpressionGene = branch[g,],
+        gene = g
+      )
+      return(gdf_list_1)
+    }))
+    gdf$branch = rep(names(branchData), times = unlist(lapply(branchData, ncol)))
+    return(gdf)
+  }, simplify = FALSE)
+
+  gdf = do.call(rbind, gdf_list)
+
+  if (!is.null(subsetBranch)) {
+    gdf_sub = subset(gdf, gdf$branch %in% subsetBranch)
+    if (nrow(gdf_sub) == 0) stop("no branches with names in subsetBranch,
+                                 please re-run with correct names (should match names of branchData)")
+  } else {
+    gdf_sub = gdf
+  }
+
+
+  if (!is.null(xvals)) {
+
+    if (!(xval %in% colnames(SummarizedExperiment::colData(scHOT)))) {
+      stop("xval provided is not found in colData(scHOT)")
+    }
+    xval <- lapply(split(seq_along(branch_info), branch_info),
+                   function(idx) xval[, idx])[order(unique(branch_info))]
+    xval <- apply(as.matrix(gdf_sub), 1, function(x)xvals[[x["branch"]]][as.numeric(x["order"])])
+    gdf_sub$order <- xval
+
+  }
+
+  g = ggplot(gdf_sub, aes(x = order, y = gdf_sub$ExpressionGene, colour = gdf_sub$gene,
+                          fill = gdf_sub$gene, linetype = gdf_sub$branch, shape = gdf_sub$branch)) +
+    geom_point() +
+    labs(fill = "Gene", col = "Gene", linetype = "Branch", shape = "Branch") +
+    theme_minimal() + geom_smooth() +
+    ylab("Expression") +
+    ggtitle(paste0(genepair, collapse = ", ")) +
+    NULL
+
+  if (facet == "branch") {
+    g = g + facet_grid(~branch)
+  }
+
+  if (facet == "gene") {
+    g = g + facet_grid(gene~.)
+  }
+
+  if (facet == "both") {
+    g = g + facet_grid(gene~branch)
+  }
+
+  return(g)
+}
+
+
 
 
